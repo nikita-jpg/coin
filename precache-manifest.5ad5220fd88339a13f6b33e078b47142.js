ddbbbdba2a82e28b24ddd3567e8905df944207ba
Nikita <nicitazaletin@yandex.ru>
2020-09-24 15:04:23 +0300

    Updates

---
+++ precache-manifest.5ad5220fd88339a13f6b33e078b47142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164d6a810b0fde9761e4fda9b01ded48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a74099a31c5cb700cfc",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "6ad7cf76b8eab53617ef",
+    "url": "./static/css/main.5e3881b5.chunk.css"
+  },
+  {
+    "revision": "2a74099a31c5cb700cfc",
+    "url": "./static/js/2.fd027ee0.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.fd027ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad7cf76b8eab53617ef",
+    "url": "./static/js/main.42302f95.chunk.js"
+  },
+  {
+    "revision": "3b759a2510ece6285d4f",
+    "url": "./static/js/runtime-main.2ac7702a.js"
+  }
+]);
